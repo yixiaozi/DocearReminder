761bdbdc676278d7b189022e73b7d21e0d193a44
yixiaozi <wangyangwoaini@gmail.com>
2022-03-06 04:12:41 +0800

    update readme

--- Info/DocearReminder/Demo/bin.mm
+++
@@ -1,6 +0,0 @@
-<map version="freeplane 1.3.0" project="17DAB3A24CC7NGK3HWY5ERX3AURZZAJ2PT99" project_last_home="file:/E:/yixiaozi/">
-<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
-<node TEXT="bin" FOLDED="false" ID="ID_899014825" CREATED="1640985472382" MODIFIED="1640985472383">
-<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
-</node>
-</map>
--- Info/DocearReminder/calander.mm
+++
@@ -1,6 +0,0 @@
-<map version="freeplane 1.3.0" project="17DAB3A24CC7NGK3HWY5ERX3AURZZAJ2PT99" project_last_home="file:/E:/yixiaozi/">
-<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
-<node TEXT="calander" FOLDED="false" ID="ID_1823483544" CREATED="1640347265596" MODIFIED="1640347265596">
-<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
-</node>
-</map>

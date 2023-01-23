8cbe8f685a375a2ee5d78623f0719cb292fcd9d3
yixiaozi <wangyangwoaini@gmail.com>
2023-01-24 06:33:16 +0800

    。

--- DevpConsole/Program.cs
+++ DevpConsole/Program.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Todoist.Net;
+using Todoist.Net.Models;
 
 namespace DevpConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-
+            ITodoistClient client = new TodoistClient("");
+            var quickAddItem = new QuickAddItem("新建任务" + "");
+            //var task = await client.Items.QuickAddAsync(quickAddItem);
+            var project =client.Projects.GetAsync();
         }
     }
 }

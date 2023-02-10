5e1d434a5f554de955028207931d46beeff8c52a
yixiaozi <wangyangwoaini@gmail.com>
2023-02-10 14:19:09 +0800

    .

--- DocearReminder/SupportClass/Extensions.cs
+++ DocearReminder/SupportClass/Extensions.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace DocearReminder
 {
